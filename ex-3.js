fda027773b5d9b4dd5d507698d508370ddc20ce8
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-05-16 15:22:27 +0700

    feat: js assignment

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,12 @@
 // Exercise #3: Find a Minimum Score
-let scores = [100, 20, 3, 1000];
-let minScore;
+let studentscores = [100, 20, 3, 1000];
+let minScore = studentscores[0];
 // Start coding here
+for (let i = 1; i < studentscores.length; i++) {
+    if (studentscores[i] < minScore) {
+        minScore = studentscores[i];
+    }
+}
 
 console.log(minScore);
+
